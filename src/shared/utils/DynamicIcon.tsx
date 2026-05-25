5528823c89793b05b8af708ce82d9f5a2a09909a
felix <felix@gmail.com>
2026-05-25 16:18:25 -0500

    feat: vista crear notificaciones con formulario completo y correción de historial de notificaciones

--- src/shared/utils/DynamicIcon.tsx
+++ src/shared/utils/DynamicIcon.tsx
@@ -37,6 +37,7 @@ import PendingActions from '@mui/icons-material/PendingActions';
 import Diversity1 from '@mui/icons-material/Diversity1';
 import BackupTable from '@mui/icons-material/BackupTable';
 import ContactPhone from '@mui/icons-material/ContactPhone';
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
 /**
  * Mapa de íconos permitidos.
  * Los nombres deben coincidir con los valores en la base de datos.
@@ -70,7 +71,7 @@ const ICON_MAP: Record<string, React.ElementType> = {
   // 🔸 Categoría: Aplicación
   Apps,
   Home,
-
+  CircleNotificationsIcon,
   // 🔸 Categoría: Aplicación
   DocumentScanner,
   GridView,
